d23ae9db0dc19924f7371f9e6a484f1731b88f17
Magnes12 <m4gn3s@gmail.com>
2026-01-27 13:57:23 +0100

    DB

---
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,82 @@
+﻿using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoints
+{
+    private const string GetGameEndpointName = "GetGameById";
+    
+    private static readonly List<GameDto> Games =
+    [
+        new(1, "street figher", "fight", 19.99M, new DateOnly(1992, 7, 15)),
+        new(2, "Witcher 3", "open-wrold", 29.99M, new DateOnly(2001, 7, 15)),
+        new(3, "Astro Bot", "platf", 49.99M, new DateOnly(2012, 7, 15))
+    ];
+
+    public static void MapGamesEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("/games");
+        
+        // GET /games
+        group.MapGet("/", () => Games)
+            .WithName("GetGames");
+
+        // GET /games/{id}
+        group.MapGet("/{id}", (int id) =>
+            {
+                var game = Games.Find(game => game.Id == id);
+        
+                return game is null ? Results.NotFound() : Results.Ok(game);
+            })
+            .WithName(GetGameEndpointName);
+
+        // POST /games
+        group.MapPost("/", (CreateGameDto newGame) =>
+        {
+            // if (string.IsNullOrEmpty(newGame.Name))
+            // {
+            //     return Results.BadRequest("Name is required");
+            // }
+            
+            GameDto game = new(
+                Games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+
+            Games.Add(game);
+    
+            return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
+        });
+
+        // PUT /games/{id}
+        group.MapPut("/{id}", (int id, UpdateGameDto updatedGames) =>
+        {
+            var index = Games.FindIndex(game => game.Id == id);
+
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+
+            Games[index] = new GameDto(
+                id,
+                updatedGames.Name,
+                updatedGames.Genre,
+                updatedGames.Price,
+                updatedGames.ReleaseDate
+            );
+    
+            return Results.NoContent();
+        });
+
+        // DELETE /games/{id}
+        group.MapDelete("/{id}", (int id) =>
+        {
+            Games.RemoveAll(game => game.Id == id);
+            return Results.NoContent();
+        });
+    }
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,62 +1,17 @@
-using GameStore.Api.Dtos;
+using GameStore.Api.Data;
+using GameStore.Api.Endpoints;
 
-const string getGameEndpointName = "GetGameById";
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddValidation();
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
-List<GameDto> games =
-[
-    new(1, "street figher", "fight", 19.99M, new DateOnly(1992, 7, 15)),
-    new(2, "Witcher 3", "open-wrold", 29.99M, new DateOnly(2001, 7, 15)),
-    new(3, "Astro Bot", "platf", 49.99M, new DateOnly(2012, 7, 15))
-];
+app.MapGamesEndpoints();
 
-// GET /games
-app.MapGet("/games", () => games)
-    .WithName("GetGames");
-
-// GET /games/{id}
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
-    .WithName(getGameEndpointName);
-
-// POST /games
-app.MapPost("/games", (CreateGameDto newGame) =>
-{
-    GameDto game = new(
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-    );
-
-    games.Add(game);
-    
-    return Results.CreatedAtRoute(getGameEndpointName, new {id = game.Id}, game);
-});
-
-// PUT /games/{id}
-app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGames) =>
-{
-    var index = games.FindIndex(game => game.Id == id);
-
-    games[index] = new GameDto(
-        id,
-        updatedGames.Name,
-        updatedGames.Genre,
-        updatedGames.Price,
-        updatedGames.ReleaseDate
-        );
-    
-    return Results.NoContent();
-});
-
-// DELETE /games/{id}
-app.MapDelete("/games/{id}", (int id) =>
-{
-    games.RemoveAll(game => game.Id == id);
-    return Results.NoContent();
-});
-
+app.MigrateDb();
 
 app.Run();
